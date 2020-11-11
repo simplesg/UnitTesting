e2fa3b4d0626434dbd1cad74ad4ac743663ca96f
Sorin <sorin19981@gmail.com>
2020-11-11 16:12:37 +0200

    modified WishListRestControllerTest

--- src/main/java/com/internship/bookstore/repository/WishListRepository.java
+++ src/main/java/com/internship/bookstore/repository/WishListRepository.java
@@ -1,6 +1,5 @@
 package com.internship.bookstore.repository;
 
-import com.internship.bookstore.model.User;
 import com.internship.bookstore.model.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
